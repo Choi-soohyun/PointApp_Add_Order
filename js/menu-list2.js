e142efcb4494f768783504e96792f83b82edea72
Choi-soohyun <thankful.t.u@gmail.com>
2020-06-01 20:00:19 +0900

    Menu를 한번에 뿌려주는데 스크롤에 따라 카테고리 메뉴를 선택할 수 있게끔 Map, Scroll Event 사용

--- js/menu-list2.js
+++ js/menu-list2.js
@@ -1,3 +1,7 @@
+let gScrollFixedFlag = 0;
+let gMenuOffsetTop = new Map();
+const cateAll = document.querySelectorAll('.cate li');
+
 const init = () => {
 	// Search TextBox Click Event
 	const txtSearch = document.getElementById('txtSearch');
@@ -16,14 +20,76 @@ const init = () => {
 		e.addEventListener("click", addCart, false); 
 	});
 
+
+	const menuTitle = document.querySelectorAll('.menu h3');
+	let sumHeight = 0;
+	[].forEach.call(menuTitle, function(e) {
+		// console.dir(e.dataset.cateNumber);
+		// console.dir(e);
+		gMenuOffsetTop.set(e.dataset.cateNumber*1, sumHeight);
+		sumHeight += e.nextElementSibling.offsetHeight + 50;
+		// console.log(e.nextElementSibling.offsetHeight, " sumHeight => ", sumHeight);
+
+	});
+
+	window.addEventListener('scroll', scrollEvent);
 	// 주소 클릭 할 때 주소 복사 기능 넣기 
 }
 
-const scrollEvent = () => {
-	// 스크롤 어느 위치 내려가면 nav, search, cate는 고정으로 보여주기
-	// 맨 상단으로 올라오면 다시 shop 정보도 보이기
-	// 
+const scrollEvent = (e) => {
+	const currentScroll = Math.round(e.target.scrollingElement.scrollTop);
+	if(currentScroll > 140 && gScrollFixedFlag === 0) {
+		// 스크롤 이벤트는 자주 일어나므로, 최대한 지역변수로 진행
+		const navScrollFixed = document.querySelector('.nav');
+		const cateScrollFixed = document.querySelector('.cate');
+		const menuScrollFixed = document.querySelector('.menu');
+
+		navScrollFixed.classList.add('nav-scroll-fixed');
+		cateScrollFixed.classList.add('cate-scroll-fixed');
+		menuScrollFixed.classList.add('menu-scroll-fixed');
+
+		gScrollFixedFlag = 1;
+	} else if(currentScroll <= 140 && gScrollFixedFlag === 1) {
+		const navScrollFixed = document.querySelector('.nav');
+		const cateScrollFixed = document.querySelector('.cate');
+		const menuScrollFixed = document.querySelector('.menu');
+
+		navScrollFixed.classList.remove('nav-scroll-fixed');
+		cateScrollFixed.classList.remove('cate-scroll-fixed');
+		menuScrollFixed.classList.remove('menu-scroll-fixed');
+
+		gScrollFixedFlag = 0;
+	}
 	// 스크롤 위치에 따라서 해당 카테고리 선택해주기
+	
+	// const menuTitle = document.querySelectorAll('.menu h3');
+	// const cateAll = document.querySelectorAll('.cate li');
+	// // 메뉴 타이틀의 offsetTop 이 현재 스크롤 넘으면 메뉴 타이틀의 data-cate의 넘버랑 cata data-cate의 넘버랑 같으면 select 해주기
+	// [].forEach.call(cateAll, function(e) {
+	// 	console.dir(e);
+	// });
+	// 
+	let selectCate = 0;
+	for(let i=0; i<gMenuOffsetTop.size; i++) {
+		cateAll[i].classList.remove('cate-selected');
+		if(currentScroll > gMenuOffsetTop.get(i+1)) {
+			selectCate = i;
+			// console.log(i, ' 현재 위치--> ', currentScroll, ' H3 태그의 위치', gMenuOffsetTop.get(i+1));
+		}
+	}
+
+
+	// const documentHeight = document.scrollingElement.offsetHeight;
+	const scrollHeight = document.scrollingElement.scrollHeight;
+	const windowHeight = document.scrollingElement.clientHeight;
+
+	if ((scrollHeight - windowHeight) <= currentScroll) {
+		// 맨 하단이면 맨 마지막 카테고리에 주기 
+		cateAll[gMenuOffsetTop.size-1].classList.add('cate-selected');
+	} else {
+		// 그게 아니면
+		cateAll[selectCate].classList.add('cate-selected');
+	}
 }
 
 const searchHistory = (e) => {
